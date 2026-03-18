4f35844ebe5806342d198b24835e2ce7a8db1926
maurotyudea <131413902+maurotyudea@users.noreply.github.com>
2026-03-17 21:22:18 -0500

    Add files via upload
    tablero y avaces en el juego

--- DesafioUno/main.cpp
+++ DesafioUno/main.cpp
@@ -1,58 +1,18 @@
-#include <iostream>
+#include <QCoreApplication>
+#include <QTextStream>
+#include "juego.h"
 
-using namespace std;
+int main(int argc, char* argv[])
+{
+    QCoreApplication app(argc, argv);
 
-int ancho;
-int alto;
+    QTextStream in(stdin);
+    QTextStream out(stdout);
 
-unsigned char* tablero;
 
-void imprimirTablero() {
+    out.flush();
 
-    for(int i = 0; i < alto; i++) {
-
-        unsigned char filaTemp = tablero[i];
-
-        for(int j = ancho-1; j >= 0; j--) {
-
-            if(filaTemp & (1 << j))
-                cout << "#";
-            else
-                cout << ".";
-        }
-
-        cout << endl;
-    }
-}
-
-void crearTablero() {
-
-    tablero = new unsigned char[alto];
-
-    for(int i = 0; i < alto; i++)
-        tablero[i] = 0;
-}
-
-int main() {
-
-    cout << "Ingrese ancho del tablero (8): ";
-    cin >> ancho;
-
-    cout << "Ingrese alto del tablero: ";
-    cin >> alto;
-
-    if(ancho != 8 || alto < 8) {
-
-        cout << "Dimensiones invalidas\n";
-        return 0;
-    }
-
-    crearTablero();
-
-    cout << "\nTablero creado:\n";
-    imprimirTablero();
-
-    delete[] tablero;
+    ejecutarJuego(in, out);
 
     return 0;
 }
